e577f86840e6d767b71c72512ce27ce981b83419
Wanessa <wanessacpr@gmail.com>
2024-11-14 23:31:26 -0300

    feat: adiciona verificacao de formulario

--- index.js
+++ index.js
@@ -4,25 +4,49 @@ const videos = [
     {titulo: 'titulo do video', duracao: 1420, scrIcone: '"teste.jpg"' }
 ]
 
-function botaoPronto(event) {
-    event.preventDefault()
-    const diasSemana = [
-        {minutos : event.target.segunda.value, dia: 'Segunda', id:'segundaResultado'}, 
-        {minutos: event.target.terca.value, dia: 'Terça', id:'tercaResultado'},
-        {minutos: event.target.quarta.value, dia: 'Quarta', id:'quartaResultado'}
-    ]
+//TODO: add restante
+const diasSemana = {
+    segunda: {minutos: null, label: 'Segunda', id: 'segundaResultado'},
+    terca: {minutos: null, label: 'Terça', id: 'tercaResultado'},
+}
 
-    diasSemana.forEach((item) => {
-        const dia = document.getElementById(item.id)
+function validaForm(event) {
+    event.preventDefault()
+    const tema = event.target.tema.value
+    var x = document.forms["formDias"]['tema'].value;
+    if (x == "") {
+      alert("Campo 'Tema' deve estar preenchido");
+      return false;
+    }
+
+    Object.keys(diasSemana).forEach(dia => {
+        const valorValido = event.target[dia].value
+        if (valorValido === '') {
+            alert("Campo '" + diasSemana[dia].label + "' deve estar preenchido");
+            return false;
+          }
+        diasSemana[dia].minutos = event.target[dia].value
+    })
+    formPronto(tema, diasSemana)
+}
+
+function formPronto(tema, diasSemana) {
+    const tituloTema = document.createElement('h1')
+
+    tituloTema.innerHTML = 'Exibindo vídeos do com o tema ' + tema
+    document.getElementById("resultados").appendChild(tituloTema)
+
+    Object.keys(diasSemana).forEach((dia) => {
+        const divDia = document.getElementById(diasSemana[dia].id)
         const titulo = document.createElement('h2')
 
-        titulo.innerHTML = item.dia + ': ' + item.minutos + ' minutos'
-        dia.appendChild(titulo) 
+        titulo.innerHTML = diasSemana[dia].label + ': ' + diasSemana[dia].minutos + ' minutos'
+        divDia.appendChild(titulo) 
 
-        const segundosDia = Number(item.minutos) * 60
+        const segundosDia = Number(diasSemana[dia].minutos) * 60
         const videos = criaGrupoVideos(segundosDia)
-        dia.appendChild(videos) 
-        document.getElementById("resultados").appendChild(dia)
+        divDia.appendChild(videos) 
+        document.getElementById("resultados").appendChild(divDia)
 
         
     })
@@ -41,6 +65,7 @@ function criaGrupoVideos(segundosDia) {
         const divVideo = document.createElement('div')
         const tituloVideo = document.createElement('p')
         const imgVideo = document.createElement('img')
+        //TODO: formatacao de segundos
         const duracaoMinutos = Number(item.duracao) / 60
 
         imgVideo.src = "teste.jpg";
@@ -55,4 +80,4 @@ function criaGrupoVideos(segundosDia) {
     return grupoVideos
 }
 
-document.getElementById("tempoDias").addEventListener("submit", botaoPronto)
+document.getElementById("formDias").addEventListener("submit", validaForm)
